1eec0645a33cc9259bf7b716e212960892f99996
lusheng <ls15555379232@163.com>
2019-06-16 21:09:46 +0800

    text

--- textgit/src/cn/itcast/controller/git.java
+++ textgit/src/cn/itcast/controller/git.java
@@ -4,7 +4,7 @@ public class git {
     public static void main(String[] args) {
         System.out.println("git成功了...");
         System.out.println("git成功了...");
-        System.out.println("git成功了...");
+        System.out.println("git失败了·...");
         System.out.println("git成功了...");
         System.out.println("git成功了...");
     }
